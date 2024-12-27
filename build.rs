bf2529ab345551277e51983f4e5a6313e51d3967
iliana etaoin <iliana@buttslol.net>
2024-12-27 11:00:34 -0800

    your regular dependency update

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
-use std::fs::File;
+use fs_err::File;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::process::Command;
-use zip::ZipWriter;
+use zip::write::{SimpleFileOptions, ZipWriter};
 
 fn main() {
     let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
@@ -17,11 +17,13 @@ fn main() {
 
     let mut writer = ZipWriter::new(File::create(out_dir.join("source.zip")).unwrap());
     for path in String::from_utf8(output.stdout).unwrap().lines() {
-        if path == "Cargo.toml.orig" {
+        if matches!(path, "Cargo.toml.orig" | ".cargo_vcs_info.json") {
             continue;
         }
 
-        writer.start_file(path, Default::default()).unwrap();
+        writer
+            .start_file(path, SimpleFileOptions::default())
+            .unwrap();
         io::copy(
             &mut File::open(Path::new(env!("CARGO_MANIFEST_DIR")).join(path)).unwrap(),
             &mut writer,
